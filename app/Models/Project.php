2ba1bd06ef13df50e1619cbd724682692a4b9cc9
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-03-01 16:31:22 +0100

    first commit

---
+++ app/Models/Project.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;
+class Project extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'type_id', 'description', 'image', 'creation_date', 'slug'];
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
+}
